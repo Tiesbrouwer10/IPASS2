75204b27396292c20f37b64321f61c8d56684990
tiesbrouwer10 <ties1000@live.nl>
2018-06-21 17:58:45 +0200

    first commit

---
+++ Afst_Sensor.hpp
@@ -0,0 +1,17 @@
+#ifndef AFST_SENSOR_HPP
+#define AFST_SENSOR_HPP
+#include "hwlib.hpp"
+#include "IR_Sensor.hpp"
+
+class Afst_Sensor{
+    hwlib::target::pin_out trigger;
+    hwlib::target::pin_in echo;
+public:
+    Afst_Sensor(hwlib::target::pin_out &trigger, hwlib::target::pin_in &echo);
+
+    int getDist();
+    void checkPerson(hwlib::target::pin_out &seenLed, IR_Sensor &infra0);
+};
+    Afst_Sensor create_Afst_Sensor();
+
+#endif // AFST_SENSOR_HPP
