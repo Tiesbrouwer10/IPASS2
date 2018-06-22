0fefccd12b82b1a134491df3a6e040719570f865
Tiesbrouwer10 <37109378+Tiesbrouwer10@users.noreply.github.com>
2018-06-22 16:01:31 +0200

    Update Servo.hpp

--- Servo.hpp
+++ Servo.hpp
@@ -7,16 +7,44 @@ class Servo{
 private:
     hwlib::target::pin_out & pin_place;
 public:
+
+    /// \brief
+    /// Servo constructor
+    /// \details
+    /// This function functions as the contructor for when a Servo object is created..
     Servo(hwlib::target::pin_out & pin_place):
        pin_place(pin_place)
        {}
-       
+    /// \brief
+    /// Turn to position 0
+    /// \details
+    /// This function moves the position of the servo to its 0 degrees position.   
     void turn_to_0();
+    
+    /// \brief
+    /// Turn to position 90
+    /// \details
+    /// This function moves the position of the servo to its 90 degrees position.  
     void turn_to_90();
+    
+    /// \brief
+    /// Turn to position 180
+    /// \details
+    /// This function moves the position of the servo to its 180 degrees position.  
     void turn_to_180();
-    void full_Head_Turn();
+    
+    /// \brief
+    /// Turn servo full
+    /// \details
+    /// This function moves the position of the servo to its 0 degrees position, afterwhich it will go to its 90 degrees position, followed bij the 180 degrees position.  
+    void full_Servo_Turn();
 };
 
+
+/// \brief
+/// Create Servo object
+/// \details
+/// When called, this function will create a Servo Object with the clock pulse on d9.
 Servo create_Servo();
     
 
